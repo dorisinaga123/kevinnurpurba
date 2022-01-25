b2aee0e71124dbd29cd01214a06d3552eece7dc1
dorisinaga123 <98421241+dorisinaga123@users.noreply.github.com>
2022-01-25 07:01:55 -1200

    upload
    upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = [' Universitas Budidarma', ' S1-Teknik Informatika', ' Fakultas Ilmu Komputer & Teknologi Informasi'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+})();
